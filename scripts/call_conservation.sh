269d2ed0d86bfa00d42fe3385508479d8c2e927e
ascheben <29115818+ascheben@users.noreply.github.com>
2022-12-02 08:59:24 -0500

    add output file to call_conservation.sh

--- scripts/call_conservation.sh
+++ scripts/call_conservation.sh
@@ -1,4 +1,4 @@
 #Fit a neutral model using the entire alignment of the IFN region
 phyloFit --out-root ifn_neutral --tree data/annotation/IFN.newick --subst-mod REV --msa-format MAF steps-output/ifn.maf
 # Conduct a likelihood ratio test for conservation across the alignment
-phyloP --method LRT --mode CONACC --wig-scores ifn_neutral.mod steps-output/ifn.maf
+phyloP --method LRT --mode CONACC --wig-scores ifn_neutral.mod steps-output/ifn.maf > ifn_phylop.wig
